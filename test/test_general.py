23a746a25eac86140f877e39fcc60212bc177d76
Diego Elian Thomas Uribe <dthomas@udemedellin.edu.co>
2025-02-26 19:39:37 -0500

    Changes to be committed: 	new file:   .gitignore 	modified:   test/test_general.py

--- test/test_general.py
+++ test/test_general.py
@@ -1,7 +1,9 @@
-
 import pytest
 
-from src.tc import TASA_USURA_EA, tarjeta_credito
+from src.tc import TASA_USURA_EA, tarjeta_credito, get_tasa_usura_mv
 
-def test_tea():
+def test_calcular_tasa_usura_efectiva_anual():
    assert TASA_USURA_EA == 0.2489
+
+def test_calcular_tasa_usura_mensual_vencida():
+   assert get_tasa_usura_mv() == 0.018694525225513958
